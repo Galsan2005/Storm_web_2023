f31bd3c9d70cf0b2dcda2e8f4ebe8129c800f6f8
Storm-5 <storm-5@Admin-5s-iMac.local>
2023-10-26 13:30:31 +0800

    add: lesson day 20

---
+++ Lesson_day_20/js/array_script.js
@@ -0,0 +1,16 @@
+console.log('Lesson Day 20 - JS Arrys');
+
+var myNumbers = [5, 8, 103 , 32, 7];
+console.log(myNumbers);
+
+var shoppingList = ['milk', 'eggs', 'bread', 'cheese', 'butter'];
+console.log(shoppingList);
+
+var randomList = [5, 7, 'milk', 'eggs', true, false];
+console.log(randomList);
+
+console.log(myNumbers[2]);
+
+console.log(shoppingList[3]);
+shoppingList[3] = 'meat';
+console.log(shoppingList[3]);
---
+++ Lesson_day_20/js/codehs.js
@@ -0,0 +1,13 @@
+ //Write your prompts here
+ var myName = prompt('What is your name?');
+ var favoriteSnack = prompt("What is your favorite snack?");
+ var favoriteTv = prompt("What's your favorite tv show?");
+ 
+ var welcome = document.getElementById("welcome");
+ console.log(welcome)
+ var snack = document.getElementById("snack");
+ var tvshow = document.getElementById("tv-show");
+ 
+ welcome.innerHTML = "welcome " + myName;
+ snack.innerHTML = "Your favorite snack is " + favoriteSnack;
+ tvshow.innerHTML = "Your favorite tv-show is " + favoriteTv;
