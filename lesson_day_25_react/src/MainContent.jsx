af010e701e2c997ea4c2d6694a48b38f6819214c
Tornado-9 <tornado-9@Admin-9s-iMac.local>
2023-11-13 16:10:01 +0800

    add: lesson day 26

---
+++ lesson_day_25_react/src/MainContent.jsx
@@ -0,0 +1,22 @@
+const MainContent = () => {
+    return (
+        <main>
+            <section id="home">
+                <h1>Welcome to My Website</h1>
+                <p>This is the home section</p>
+            </section>
+        
+            <section id="about">
+                <h1>About Us</h1>
+                <p>Information about the project or website.</p>
+            </section>
+
+            <section id="contact">
+                <h1>Contact Us</h1>
+                <p>Details on how to contact.</p>
+            </section>
+        </main>
+    );
+};
+
+export default MainContent;
