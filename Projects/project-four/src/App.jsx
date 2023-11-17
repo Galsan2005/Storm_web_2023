4767163f6beb61f7b09f90da49cdf094a3cffcc1
Tornado-9 <tornado-9@Admin-9s-iMac.local>
2023-11-17 17:34:14 +0800

    add: lesson day 28 react

---
+++ Projects/project-four/src/App.jsx
@@ -0,0 +1,13 @@
+import Layout from "./component/Layout";
+
+
+function App() {
+
+  return (
+    <div className='bg-blue-400 w-360 h-450.5'>
+      <Layout />
+    </div>
+  );
+};
+
+export default App;
---
+++ Projects/project-four/src/component/Layout.jsx
@@ -0,0 +1,10 @@
+import Sidebar from "./Sidebar";
+
+const Layout = () => {
+    return (
+        <div>
+            <Sidebar />
+        </div>
+    )
+}
+export default Layout;
