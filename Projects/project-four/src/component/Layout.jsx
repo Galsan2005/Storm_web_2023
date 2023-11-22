df340f7287d57d586db28217b7beae4fda0122bf
Tornado-9 <tornado-9@Admin-9s-iMac.local>
2023-11-22 19:00:59 +0800

    add: project 4

--- Projects/project-four/src/component/Layout.jsx
+++ Projects/project-four/src/component/Layout.jsx
@@ -1,9 +1,11 @@
+import Main from "./Main";
 import Sidebar from "./Sidebar";
 
 const Layout = () => {
     return (
-        <div>
+        <div className="flex w-[1440px] h-[1802px]">
             <Sidebar />
+            <Main />
         </div>
     )
 }
