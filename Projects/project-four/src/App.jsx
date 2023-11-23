70075ffb5fa8cb1f9710b8275f03ef8cbd511a75
Temuulen Gerelsaikhan <temuulengerelsaikhan@TemuulennoMacBook-puro.local>
2023-11-23 23:35:30 +0800

    add project four

--- Projects/project-four/src/App.jsx
+++ Projects/project-four/src/App.jsx
@@ -4,7 +4,7 @@ import Layout from "./component/Layout";
 function App() {
 
   return (
-    <div className='bg-blue-400 w-360 h-450.5'>
+    <div className='bg-blue-400 w-360 h-340'>
       <Layout />
     </div>
   );
--- Projects/project-four/src/component/Sidebar.jsx
+++ Projects/project-four/src/component/Sidebar.jsx
@@ -1,6 +1,6 @@
 const Sidebar = () => {
     return (
-        <div>Sidebar</div>
+        <div className="inline-flex h-460 px-9 py-40">Sidebar</div>
     )
 }
 export default Sidebar;
