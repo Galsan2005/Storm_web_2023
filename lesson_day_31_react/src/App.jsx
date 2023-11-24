178daeba2526c47bc8d407257537e337adacf23a
Tornado-9 <tornado-9@Admin-9s-iMac.local>
2023-11-24 15:17:17 +0800

    add: lesson day 31

---
+++ lesson_day_31_react/src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Use State in ReactJS</h1>
+      <header className='App-header'>
+        <Counter />
+      </header>
+    </div>
+  );
+}
+
+export default App
