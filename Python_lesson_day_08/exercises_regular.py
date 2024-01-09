4da8ca85034ac326d9aec7523c8fdb9737e65ba2
Temuulen Gerelsaikhan <temuulengerelsaikhan@TemuulennoMacBook-puro.local>
2024-01-09 18:16:15 +0800

    add: experiment tuples

--- Python_lesson_day_08/exercises_regular.py
+++ Python_lesson_day_08/exercises_regular.py
@@ -13,20 +13,29 @@
 
 # print("mbox.txt had ", count ,"lines thath matched ", word)
 
-print('===========')
-print('exercise 02')
-print('===========')
+# print('===========')
+# print('exercise 02')
+# print('===========')
 
-import re
-name = str(input('Enter file: '))
-count = 0 
-sum = 0
-hand = open(name)
-for line in hand:
-    line = line.rstrip()
-    x = re.findall('^New Revision: ([0-9.]+)', line)
-    if len(x) > 0:
-        count +=1
-        sum = sum + int(x[0])
 
-print(int(sum/count))
+# import re
+# name = str(input('Enter file: '))
+# count = 0 
+# sum = 0
+# hand = open(name)
+# for line in hand:
+#     line = line.rstrip()
+#     x = re.findall('^New Revision: ([0-9.]+)', line)
+#     if len(x) > 0:
+#         count +=1
+#         sum = sum + int(x[0])
+
+# print(int(sum/count))
+
+list_experiment = dict()
+fhand = "galsan"
+for line in fhand:
+    list_experiment[line] = list_experiment.get(line, 0) + 1
+t = ['a','1', 'c', 'b']
+t.sort()
+print(t)
